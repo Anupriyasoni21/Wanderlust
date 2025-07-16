4e215876a228126d9aaffc690e0d7c9886b311b3
Anupriya soni <sonianupriya204@gmail.com>
2025-07-16 16:41:20 +0530

    Update geocoder.js

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -2,9 +2,6 @@ const NodeGeocoder = require('node-geocoder');
 
 const options = {
   provider: 'openstreetmap',
-  httpAdapter: 'https', 
-  userAgent: 'wanderlust-app (sonianupriya204@gmail.com)',
-  formatter: null
 };
 
 const geocoder = NodeGeocoder(options);
